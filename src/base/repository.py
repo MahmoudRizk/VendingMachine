e3faeca5d80ce800f7c81bb6b2a6ccf6d760594b
MahmoudRizk <mahmoud.rizk@lavaloon.com>
2022-04-01 00:27:18 +0200

    Refactor user repository.

--- src/base/repository.py
+++ src/base/repository.py
@@ -1,2 +1,55 @@
+from typing import Optional, Type
+
+from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm import Session
+
+from src.base.db_model import DbModel
+from src.base.domain import Domain
+from src.base.mapper import Mapper
+
+
 class Repository:
-    pass
+    def __init__(self, engine, mapper: Mapper, db_model_type: Type[DbModel], domain_model_type: Type[Domain]):
+        self.engine = engine
+        self.mapper = mapper
+        self.db_model_type = db_model_type
+        self.domain_model_type = domain_model_type
+
+    def insert(self, domain_model: Domain) -> Domain:
+        if not domain_model.id:
+            domain_model.set_uuid()
+
+        with Session(self.engine) as session:
+            db_model = self.mapper.domain_to_data(domain_model.to_dict(), self.db_model_type)
+
+            stmt_res = self._get_by_id(session=session, _id=db_model.id)
+
+            if not stmt_res:
+                # Insert new record
+                session.add(db_model)
+                res = self.mapper.data_to_domain(db_model.__dict__, self.domain_model_type)
+            else:
+                # Update Existing record
+                stmt_res.__dict__.update(db_model.__dict__)
+                res = self.mapper.data_to_domain(stmt_res.__dict__, self.domain_model_type)
+
+            session.commit()
+            return res
+
+    def get_by_id(self, _id: str) -> Optional[Domain]:
+        with Session(self.engine) as session:
+            db_model: Optional[DbModel] = self._get_by_id(session=session, _id=_id)
+            if db_model:
+                return self.mapper.data_to_domain(db_model.__dict__, self.domain_model_type)
+
+            return None
+
+    def _get_by_id(self, session, _id: str) -> Optional[Domain]:
+        stmt = select(self.db_model_type).where(self.db_model_type.id == _id)
+        try:
+            stmt_res = session.scalars(stmt).one()
+        except NoResultFound as e:
+            stmt_res = None
+
+        return stmt_res
--- src/user_repository.py
+++ src/user_repository.py
@@ -1,54 +1,12 @@
-from typing import Optional
-
-from sqlalchemy import select
-from sqlalchemy.exc import NoResultFound
-
+from src.base.repository import Repository
 from src.mapper import UserMapper
 from user import User
 from db_user import DbUser
 
-from sqlalchemy.orm import Session
 
-
-class UserRepository:
+class UserRepository(Repository):
     def __init__(self, engine, mapper: UserMapper):
-        self.mapper = mapper
-        self.engine = engine
-
-    def insert(self, user: User) -> User:
-        if not user.id:
-            user.set_uuid()
-
-        with Session(self.engine) as session:
-            db_user = self.mapper.domain_to_data(user.to_dict(), DbUser)
-
-            stmt_res = self._get_by_id(session=session, _id=db_user.id)
-
-            if not stmt_res:
-                # Insert new record.
-                session.add(db_user)
-                res = self.mapper.data_to_domain(db_user.__dict__, User)
-            else:
-                # Update Existing record.
-                stmt_res.__dict__.update(db_user.__dict__)
-                res = self.mapper.data_to_domain(stmt_res.__dict__, User)
-
-            session.commit()
-            return res
-
-    def get_by_id(self, _id: str) -> User:
-        with Session(self.engine) as session:
-            db_user: Optional[DbUser] = self._get_by_id(session=session, _id=_id)
-            if db_user:
-                return self.mapper.data_to_domain(db_user.__dict__, User)
-
-            return None
-
-    def _get_by_id(self, session, _id: str) -> Optional[DbUser]:
-        stmt = select(DbUser).where(DbUser.id == _id)
-        try:
-            stmt_res = session.scalars(stmt).one()
-        except NoResultFound as e:
-            stmt_res = None
-
-        return stmt_res
+        db_model_type = DbUser
+        domain_model_type = User
+        super(UserRepository, self).__init__(engine=engine, mapper=mapper, db_model_type=db_model_type,
+                                             domain_model_type=domain_model_type)
